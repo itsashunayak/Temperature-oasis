c5bc5a7a13a77aa94e8a01c8a3e17eb4dd9c3c97
Ashutosh Nayak <rinku26031@gmail.com>
2022-10-01 00:49:51 +0530

    01-10-22

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ let temprature = () => {
     if (opt.value == "Celsius") {
         console.log(a, "C to F", opt.value)
         let F = a * (9 / 5) + 32
-        display.innerHTML = F.toFixed(4) + " F"
+        display.innerHTML = F.toFixed(2) + "ºF"
         
 
     } else {
         console.log(a, "F to C", opt.value)
         let C = (a - 32) * (5 / 9)
-        display.innerHTML = C.toFixed(4) + " ºC"
+        display.innerHTML = C.toFixed(2) + " ºC"
         
     }
 }
